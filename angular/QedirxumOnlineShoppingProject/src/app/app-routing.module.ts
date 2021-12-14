c043c2e344c207d093d887820c70ff9ce5246543
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-12-14 20:05:35 +0400

    admin girende admin panel gorsenmesi

--- angular/QedirxumOnlineShoppingProject/src/app/app-routing.module.ts
+++ angular/QedirxumOnlineShoppingProject/src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LogoutComponent } from './logout/logout.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AdminPanelComponent}  from './admin-panel/admin-panel.component'
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminPanelComponent },
 
   { path: 'home', component: HomePageComponent },
   { path: 'logout', component: LogoutComponent },
