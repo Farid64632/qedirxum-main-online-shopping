ad46940ce7b0bb87847e3d3f6100f6f243912961
Ferid223 <ferid000m@gmail.com>
2021-12-14 19:09:52 +0400

    Login

--- spring/shopping/shopping/src/main/java/az/qedirxum/shopping/model/UserModel.java
+++ spring/shopping/shopping/src/main/java/az/qedirxum/shopping/model/UserModel.java
@@ -3,13 +3,22 @@ package az.qedirxum.shopping.model;
 
     
 
-import lombok.Getter;
-import lombok.Setter;
-@Getter
-@Setter
+
 public class UserModel {
     private String username;
     private String password;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 
