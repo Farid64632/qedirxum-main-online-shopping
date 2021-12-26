b3e0e33dd26331ed5a26914e4a92255a2d96b205
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-12-26 19:45:51 +0400

    Create food.ts

---
+++ angular/QedirxumOnlineShoppingProject/src/app/models/food.ts
@@ -0,0 +1,9 @@
+export class Food {
+    public id: number = 0;
+    public price: number = 0;
+    public name: string = '';
+ public description: string = '';
+    public miqdar: string = '';
+      public image: string = '';
+  
+  }
