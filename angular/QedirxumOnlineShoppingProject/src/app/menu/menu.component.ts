5479bab52feca76ba87dfdc8958ab20a44689822
cavid-huseynzade-7675 <alicavid826@gmail.com>
2022-01-08 14:47:53 +0400

    animation toolbar

--- angular/QedirxumOnlineShoppingProject/src/app/menu/menu.component.ts
+++ angular/QedirxumOnlineShoppingProject/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LoginService } from '../service/login.service';
@@ -6,11 +7,25 @@ import { LoginService } from '../service/login.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  animations: [
+		trigger('inOutAnimation', [
+			transition(':enter', [
+				style({ height: 0, opacity: 0 }),
+				animate('0.4s ease-out', style({ height: 300, opacity: 1 }))
+			]),
+			transition(':leave', [
+				style({ height: 300, opacity: 1 }),
+				animate('0.4s ease-in', style({ height: 0, opacity: 0 }))
+			])
+		])
+	]
 })
 export class MenuComponent implements OnInit {
 roles:string[]=[];
 userLoggedin:boolean=false;
+visiblescroll:boolean=true;
+
   constructor(private loginSerice:LoginService,private router:Router) { }
 
   ngOnInit(): void {
@@ -34,5 +49,16 @@ this.loginSerice.userRoles.subscribe(
 
   toManager(){
     this.router.navigate(['manager-t']);
+  }
+
+  @HostListener('window:scroll',['$event'])   onscroll(){
+    console.log("girdi")
+    if (window.scrollY>100) {
+      this.visiblescroll=false;
+      console.log("false")
+    }else{
+      this.visiblescroll=true;
+      console.log("true")
+    }
   }
 }
